08b9df7d21880e3dafdd4cdb9c1b775f62b5be54
nsavvide <niels.i.savvides@gmail.com>
2024-03-12 10:58:09 +0100

    :lipstick: Upgrade card design

--- src/components/object-list/object/object.jsx
+++ src/components/object-list/object/object.jsx
@@ -3,10 +3,9 @@ import "./object.css";
 
 function Object({ object_name }) {
   return (
-    <div class="box">
-      <span></span>
-      <div class="content">
-        <h2>{object_name}</h2>
+    <div className="card">
+      <div className="text">
+        <span>{object_name}</span>
       </div>
     </div>
   );
